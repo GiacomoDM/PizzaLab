21047fed9ed577bfffe124bc6b08cc31223ed979
Giacomo <giacomo.delmoro@gmail.com>
2018-11-05 17:29:28 +0100

    initial layout and structure of order-to-deliver component

--- src/app/orders-to-deliver/orders-to-deliver.component.ts
+++ src/app/orders-to-deliver/orders-to-deliver.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OrderService } from '../order.service';
+import { Order } from '../order';
+
 @Component({
   selector: 'app-orders-to-deliver',
   templateUrl: './orders-to-deliver.component.html',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersToDeliverComponent implements OnInit {
 
-  constructor() { }
+  orders: Order[];
+  hasErrors: boolean;
+  errorMsg: string;
+  currentPage = 1;
+  sortType = 'delivery';
+  sortReverse = true;
+
+  constructor(
+    private orderService: OrderService,
+  ) { }
 
   ngOnInit() {
+    this.getOrders();
   }
 
+  getOrders(): void {
+    this.orderService.getOrdersToDeliver().subscribe(
+      orders => {
+        this.orders = orders;
+        this.hasErrors = false;
+      },
+      err => {
+        this.hasErrors = true;
+        this.errorMsg = 'Impossibile recuperare i dati dal server.';
+      }
+    );
+  }
+
+  reverse(type: string): void {
+    if (this.sortType === type) {
+      this.sortReverse = !this.sortReverse;
+    } else {
+      this.sortReverse = false;
+    }
+  }
 }
