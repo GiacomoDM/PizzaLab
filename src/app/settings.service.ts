1a3cbb5c86495817c048ea617f09682da2338257
Giacomo <giacomo.delmoro@gmail.com>
2018-11-14 12:19:43 +0100

    update delivery times and base address on settings service

--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -14,8 +14,8 @@ const httpOptions = {
 })
 export class SettingsService {
 
-  private addressUrl = '../assets/config/baseAddress.json';
-  private timesUrl = '../assets/config/deliveryTimes.json';
+  private addressUrl = 'http://localhost:3000/baseAddress';
+  private timesUrl = 'http://localhost:3000/deliveryTimes';
 
   constructor( private http: HttpClient ) { }
 
@@ -29,6 +29,22 @@ export class SettingsService {
 
   getTimes(): Observable<any> {
     return this.http.get<any>(this.timesUrl)
+    .pipe(
+      retry(3),
+      catchError(err => throwError(new Error('')))
+    );
+  }
+
+  updateAddress(address: Address): Observable<Address> {
+    return this.http.put<Address>(this.addressUrl, address, httpOptions)
+    .pipe(
+      retry(3),
+      catchError(err => throwError(new Error('')))
+    );
+  }
+
+  updateTimes(times: any): Observable<any> {
+    return this.http.put<any>(this.timesUrl, times, httpOptions)
     .pipe(
       retry(3),
       catchError(err => throwError(new Error('')))
